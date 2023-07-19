af6d35209d5fb5125bd144c7dc860853e5a1912f
SyntaxVaneza <91642506+SyntaxVaneza@users.noreply.github.com>
2023-07-19 03:15:14 -0400

    Testing a commit function

--- full-stack-takehome copy/intsureview_fe/src/App.js
+++ full-stack-takehome copy/intsureview_fe/src/App.js
@@ -14,6 +14,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          Testing again.
         </p>
         
         <a
