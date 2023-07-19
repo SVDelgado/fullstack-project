9862014cfd40273e187ce8bd48e595204cd282be
SyntaxVaneza <91642506+SyntaxVaneza@users.noreply.github.com>
2023-07-19 03:02:30 -0400

    Created Footer/Header Base

--- full-stack-takehome copy/intsureview_fe/src/App.js
+++ full-stack-takehome copy/intsureview_fe/src/App.js
@@ -1,14 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header />
+      
+      <main>
+  
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -18,6 +25,9 @@ function App() {
           Learn React
         </a>
       </header>
+      </main>
+
+      <Footer />
     </div>
   );
 }
